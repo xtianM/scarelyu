8ad5dbe460b2611fff9e9f80f5446c60b870f554
raymund <raymund.reyes@vocphone.com>
2022-03-10 20:08:31 +0800

    storya ti kwa

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,25 @@ function linkActive(event) {
     var acat = document.getElementById("page-link-ul").getElementsByClassName('active')[0].classList.remove('active')
     event.target.parentElement.classList.add('active')
 }
+var index = 0
+function nextPage() {
+    var totalPages = document.getElementsByClassName('stories-container').length
+    document.getElementsByClassName('stories-container')[index].style.display = 'none';
+    if (totalPages - 1 == index) {
+        index = 0
+    } else {
+        index ++
+    }
+    document.getElementsByClassName('stories-container')[index].style.display = 'inline-flex';
+}
+
+function prevPage() {
+    var totalPages = document.getElementsByClassName('stories-container').length
+    document.getElementsByClassName('stories-container')[index].style.display = 'none';
+    if (index == 0) {
+        index = totalPages -1 
+    } else {
+        index --
+    }
+    document.getElementsByClassName('stories-container')[index].style.display = 'inline-flex';
+}
